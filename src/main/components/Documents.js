586d3f9a07dc1d9049106dd6bd83fbc1ddf47f2e
juanmanuelbouvier <juanmabouvier@gmail.com>
2017-11-17 21:34:00 -0300

    Agrego expedientes mockeados en pantalla de Documentos.

--- src/main/components/Documents.js
+++ src/main/components/Documents.js
@@ -3,6 +3,27 @@ import {Body, Button, Container, Content, Header, Icon, Left, List, ListItem, Ri
 import {Alert} from 'react-native';
 
 export default class Documents extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recentDocuments: [
+                {
+                    title: 'APELACION Pierri',
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                    date: '2017-10-02T18:31:02'
+                },
+                {
+                    title: 'DEMANDA Isidoro Gomez',
+                    content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+                    date: '2017-08-28T10:11:54'
+                }
+            ],
+            caseFiles: [
+                {id: '011093/2017'}, {id: '010842/2017'}, {id: '000012/2016'}
+            ]
+        }
+    }
+
     render() {
         return (
             <Container>
@@ -31,53 +52,37 @@ export default class Documents extends Component {
                     </Right>
                 </Header>
                 <Content>
-                    <List>
-                        <ListItem itemHeader first>
-                            <Text>RECIENTES</Text>
-                        </ListItem>
-                        <ListItem>
-                            <Left>
-                                <Text>APELACION Pierri</Text>
-                            </Left>
-                            <Right>
-                                <Icon name="ios-arrow-forward" />
-                            </Right>
-                        </ListItem>
-                        <ListItem>
-                            <Left>
-                                <Text>DEMANDA Isidoro Gomez</Text>
-                            </Left>
-                            <Right>
-                                <Icon name="ios-arrow-forward" />
-                            </Right>
-                        </ListItem>
-                        <ListItem itemHeader>
-                            <Text>EXPEDIENTES</Text>
-                        </ListItem>
-                        <ListItem>
-                            <Left>
-                                <Text>063209/2017</Text>
-                            </Left>
-                            <Right>
-                                <Icon name="ios-arrow-forward" />
-                            </Right>
-                        </ListItem>
-                        <ListItem>
-                            <Left>
-                                <Text>063021/2017</Text>
-                            </Left>
-                            <Right>
-                                <Icon name="ios-arrow-forward" />
-                            </Right>
-                        </ListItem>
-                        <ListItem>
-                            <Left>
-                                <Text>000012/2016</Text>
-                            </Left>
-                            <Right>
-                                <Icon name="ios-arrow-forward" />
-                            </Right>
-                        </ListItem>
+                    <ListItem itemHeader first>
+                        <Text>RECIENTES</Text>
+                    </ListItem>
+                    <List
+                        dataArray={this.state.recentDocuments}
+                        renderRow={(doc) =>
+                            <ListItem>
+                                <Left>
+                                    <Text>{doc.title}</Text>
+                                </Left>
+                                <Right>
+                                    <Icon name="ios-arrow-forward" />
+                                </Right>
+                            </ListItem>
+                        }>
+                    </List>
+                    <ListItem itemHeader>
+                        <Text>EXPEDIENTES</Text>
+                    </ListItem>
+                    <List
+                        dataArray={this.state.caseFiles}
+                        renderRow={(file) =>
+                            <ListItem>
+                                <Left>
+                                    <Text>{file.id}</Text>
+                                </Left>
+                                <Right>
+                                    <Icon name="ios-arrow-forward" />
+                                </Right>
+                            </ListItem>
+                        }>
                     </List>
                 </Content>
             </Container>
